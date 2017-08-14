846f1e7225b99ac28ae6a17f7b26958c94476c9b
David Zemon <david@zemon.name>
2017-08-14 17:45:52 -0500

    [#133] Work in progress. Pushing code to move to a different computer

---
+++ PropWare/sensor/fusion/bno055.h
@@ -0,0 +1,358 @@
+/**
+ * @file    PropWare/sensor/fusion/bno055.h
+ *
+ * @author  David Zemon
+ *
+ * @copyright
+ * The MIT License (MIT)<br>
+ * <br>Copyright (c) 2013 David Zemon<br>
+ * <br>Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:<br>
+ * <br>The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.<br>
+ * <br>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#pragma once
+
+#include <PropWare/serial/i2c/i2cmaster.h>
+
+namespace PropWare {
+
+class BNO055 {
+    public:
+        static const uint8_t ADDRESS_A = 0x28;
+        static const uint8_t ADDRESS_B = 0x28;
+        static const uint8_t ID        = 0xA0;
+
+        struct Offsets {
+            uint16_t accel_offset_x;
+            uint16_t accel_offset_y;
+            uint16_t accel_offset_z;
+            uint16_t gyro_offset_x;
+            uint16_t gyro_offset_y;
+            uint16_t gyro_offset_z;
+            uint16_t mag_offset_x;
+            uint16_t mag_offset_y;
+            uint16_t mag_offset_z;
+
+            uint16_t accel_radius;
+            uint16_t mag_radius;
+        };
+
+        typedef enum {
+            /* Page id register definition */
+                BNO055_PAGE_ID_ADDR = 0X07,
+
+            /* PAGE0 REGISTER DEFINITION START*/
+                BNO055_CHIP_ID_ADDR       = 0x00,
+                BNO055_ACCEL_REV_ID_ADDR  = 0x01,
+                BNO055_MAG_REV_ID_ADDR    = 0x02,
+                BNO055_GYRO_REV_ID_ADDR   = 0x03,
+                BNO055_SW_REV_ID_LSB_ADDR = 0x04,
+                BNO055_SW_REV_ID_MSB_ADDR = 0x05,
+                BNO055_BL_REV_ID_ADDR     = 0X06,
+
+            /* Accel data register */
+                BNO055_ACCEL_DATA_X_LSB_ADDR = 0X08,
+                BNO055_ACCEL_DATA_X_MSB_ADDR = 0X09,
+                BNO055_ACCEL_DATA_Y_LSB_ADDR = 0X0A,
+                BNO055_ACCEL_DATA_Y_MSB_ADDR = 0X0B,
+                BNO055_ACCEL_DATA_Z_LSB_ADDR = 0X0C,
+                BNO055_ACCEL_DATA_Z_MSB_ADDR = 0X0D,
+
+            /* Mag data register */
+                BNO055_MAG_DATA_X_LSB_ADDR = 0X0E,
+                BNO055_MAG_DATA_X_MSB_ADDR = 0X0F,
+                BNO055_MAG_DATA_Y_LSB_ADDR = 0X10,
+                BNO055_MAG_DATA_Y_MSB_ADDR = 0X11,
+                BNO055_MAG_DATA_Z_LSB_ADDR = 0X12,
+                BNO055_MAG_DATA_Z_MSB_ADDR = 0X13,
+
+            /* Gyro data registers */
+                BNO055_GYRO_DATA_X_LSB_ADDR = 0X14,
+                BNO055_GYRO_DATA_X_MSB_ADDR = 0X15,
+                BNO055_GYRO_DATA_Y_LSB_ADDR = 0X16,
+                BNO055_GYRO_DATA_Y_MSB_ADDR = 0X17,
+                BNO055_GYRO_DATA_Z_LSB_ADDR = 0X18,
+                BNO055_GYRO_DATA_Z_MSB_ADDR = 0X19,
+
+            /* Euler data registers */
+                BNO055_EULER_H_LSB_ADDR = 0X1A,
+                BNO055_EULER_H_MSB_ADDR = 0X1B,
+                BNO055_EULER_R_LSB_ADDR = 0X1C,
+                BNO055_EULER_R_MSB_ADDR = 0X1D,
+                BNO055_EULER_P_LSB_ADDR = 0X1E,
+                BNO055_EULER_P_MSB_ADDR = 0X1F,
+
+            /* Quaternion data registers */
+                BNO055_QUATERNION_DATA_W_LSB_ADDR = 0X20,
+                BNO055_QUATERNION_DATA_W_MSB_ADDR = 0X21,
+                BNO055_QUATERNION_DATA_X_LSB_ADDR = 0X22,
+                BNO055_QUATERNION_DATA_X_MSB_ADDR = 0X23,
+                BNO055_QUATERNION_DATA_Y_LSB_ADDR = 0X24,
+                BNO055_QUATERNION_DATA_Y_MSB_ADDR = 0X25,
+                BNO055_QUATERNION_DATA_Z_LSB_ADDR = 0X26,
+                BNO055_QUATERNION_DATA_Z_MSB_ADDR = 0X27,
+
+            /* Linear acceleration data registers */
+                BNO055_LINEAR_ACCEL_DATA_X_LSB_ADDR = 0X28,
+                BNO055_LINEAR_ACCEL_DATA_X_MSB_ADDR = 0X29,
+                BNO055_LINEAR_ACCEL_DATA_Y_LSB_ADDR = 0X2A,
+                BNO055_LINEAR_ACCEL_DATA_Y_MSB_ADDR = 0X2B,
+                BNO055_LINEAR_ACCEL_DATA_Z_LSB_ADDR = 0X2C,
+                BNO055_LINEAR_ACCEL_DATA_Z_MSB_ADDR = 0X2D,
+
+            /* Gravity data registers */
+                BNO055_GRAVITY_DATA_X_LSB_ADDR = 0X2E,
+                BNO055_GRAVITY_DATA_X_MSB_ADDR = 0X2F,
+                BNO055_GRAVITY_DATA_Y_LSB_ADDR = 0X30,
+                BNO055_GRAVITY_DATA_Y_MSB_ADDR = 0X31,
+                BNO055_GRAVITY_DATA_Z_LSB_ADDR = 0X32,
+                BNO055_GRAVITY_DATA_Z_MSB_ADDR = 0X33,
+
+            /* Temperature data register */
+                BNO055_TEMP_ADDR = 0X34,
+
+            /* Status registers */
+                BNO055_CALIB_STAT_ADDR      = 0X35,
+                BNO055_SELFTEST_RESULT_ADDR = 0X36,
+                BNO055_INTR_STAT_ADDR       = 0X37,
+
+            BNO055_SYS_CLK_STAT_ADDR = 0X38,
+            BNO055_SYS_STAT_ADDR     = 0X39,
+            BNO055_SYS_ERR_ADDR      = 0X3A,
+
+            /* Unit selection register */
+                BNO055_UNIT_SEL_ADDR    = 0X3B,
+                BNO055_DATA_SELECT_ADDR = 0X3C,
+
+            /* Mode registers */
+                BNO055_OPR_MODE_ADDR = 0X3D,
+                BNO055_PWR_MODE_ADDR = 0X3E,
+
+            BNO055_SYS_TRIGGER_ADDR = 0X3F,
+            BNO055_TEMP_SOURCE_ADDR = 0X40,
+
+            /* Axis remap registers */
+                BNO055_AXIS_MAP_CONFIG_ADDR = 0X41,
+                BNO055_AXIS_MAP_SIGN_ADDR   = 0X42,
+
+            /* SIC registers */
+                BNO055_SIC_MATRIX_0_LSB_ADDR = 0X43,
+                BNO055_SIC_MATRIX_0_MSB_ADDR = 0X44,
+                BNO055_SIC_MATRIX_1_LSB_ADDR = 0X45,
+                BNO055_SIC_MATRIX_1_MSB_ADDR = 0X46,
+                BNO055_SIC_MATRIX_2_LSB_ADDR = 0X47,
+                BNO055_SIC_MATRIX_2_MSB_ADDR = 0X48,
+                BNO055_SIC_MATRIX_3_LSB_ADDR = 0X49,
+                BNO055_SIC_MATRIX_3_MSB_ADDR = 0X4A,
+                BNO055_SIC_MATRIX_4_LSB_ADDR = 0X4B,
+                BNO055_SIC_MATRIX_4_MSB_ADDR = 0X4C,
+                BNO055_SIC_MATRIX_5_LSB_ADDR = 0X4D,
+                BNO055_SIC_MATRIX_5_MSB_ADDR = 0X4E,
+                BNO055_SIC_MATRIX_6_LSB_ADDR = 0X4F,
+                BNO055_SIC_MATRIX_6_MSB_ADDR = 0X50,
+                BNO055_SIC_MATRIX_7_LSB_ADDR = 0X51,
+                BNO055_SIC_MATRIX_7_MSB_ADDR = 0X52,
+                BNO055_SIC_MATRIX_8_LSB_ADDR = 0X53,
+                BNO055_SIC_MATRIX_8_MSB_ADDR = 0X54,
+
+            /* Accelerometer Offset registers */
+                ACCEL_OFFSET_X_LSB_ADDR = 0X55,
+                ACCEL_OFFSET_X_MSB_ADDR = 0X56,
+                ACCEL_OFFSET_Y_LSB_ADDR = 0X57,
+                ACCEL_OFFSET_Y_MSB_ADDR = 0X58,
+                ACCEL_OFFSET_Z_LSB_ADDR = 0X59,
+                ACCEL_OFFSET_Z_MSB_ADDR = 0X5A,
+
+            /* Magnetometer Offset registers */
+                MAG_OFFSET_X_LSB_ADDR = 0X5B,
+                MAG_OFFSET_X_MSB_ADDR = 0X5C,
+                MAG_OFFSET_Y_LSB_ADDR = 0X5D,
+                MAG_OFFSET_Y_MSB_ADDR = 0X5E,
+                MAG_OFFSET_Z_LSB_ADDR = 0X5F,
+                MAG_OFFSET_Z_MSB_ADDR = 0X60,
+
+            /* Gyroscope Offset register s*/
+                GYRO_OFFSET_X_LSB_ADDR = 0X61,
+                GYRO_OFFSET_X_MSB_ADDR = 0X62,
+                GYRO_OFFSET_Y_LSB_ADDR = 0X63,
+                GYRO_OFFSET_Y_MSB_ADDR = 0X64,
+                GYRO_OFFSET_Z_LSB_ADDR = 0X65,
+                GYRO_OFFSET_Z_MSB_ADDR = 0X66,
+
+            /* Radius registers */
+                ACCEL_RADIUS_LSB_ADDR = 0X67,
+                ACCEL_RADIUS_MSB_ADDR = 0X68,
+                MAG_RADIUS_LSB_ADDR   = 0X69,
+                MAG_RADIUS_MSB_ADDR   = 0X6A
+        }                    Address;
+
+        typedef enum {
+            POWER_MODE_NORMAL   = 0X00,
+            POWER_MODE_LOWPOWER = 0X01,
+            POWER_MODE_SUSPEND  = 0X02
+        }                    PowerMode;
+
+        typedef enum {
+            /* Operation mode settings*/
+                OPERATION_MODE_CONFIG       = 0X00,
+                OPERATION_MODE_ACCONLY      = 0X01,
+                OPERATION_MODE_MAGONLY      = 0X02,
+                OPERATION_MODE_GYRONLY      = 0X03,
+                OPERATION_MODE_ACCMAG       = 0X04,
+                OPERATION_MODE_ACCGYRO      = 0X05,
+                OPERATION_MODE_MAGGYRO      = 0X06,
+                OPERATION_MODE_AMG          = 0X07,
+                OPERATION_MODE_IMUPLUS      = 0X08,
+                OPERATION_MODE_COMPASS      = 0X09,
+                OPERATION_MODE_M4G          = 0X0A,
+                OPERATION_MODE_NDOF_FMC_OFF = 0X0B,
+                OPERATION_MODE_NDOF         = 0X0C
+        }                    OperationMode;
+
+        typedef enum {
+            REMAP_CONFIG_P0 = 0x21,
+            REMAP_CONFIG_P1 = 0x24, // default
+            REMAP_CONFIG_P2 = 0x24,
+            REMAP_CONFIG_P3 = 0x21,
+            REMAP_CONFIG_P4 = 0x24,
+            REMAP_CONFIG_P5 = 0x21,
+            REMAP_CONFIG_P6 = 0x21,
+            REMAP_CONFIG_P7 = 0x24
+        }                    AxisRemapConfiguration;
+
+        typedef enum {
+            REMAP_SIGN_P0 = 0x04,
+            REMAP_SIGN_P1 = 0x00, // default
+            REMAP_SIGN_P2 = 0x06,
+            REMAP_SIGN_P3 = 0x02,
+            REMAP_SIGN_P4 = 0x03,
+            REMAP_SIGN_P5 = 0x01,
+            REMAP_SIGN_P6 = 0x07,
+            REMAP_SIGN_P7 = 0x05
+        }                    AxisRemapSign;
+
+        typedef struct {
+            uint8_t  accel_rev;
+            uint8_t  mag_rev;
+            uint8_t  gyro_rev;
+            uint16_t sw_rev;
+            uint8_t  bl_rev;
+        }                    RevisionInfo;
+
+        typedef enum {
+            VECTOR_ACCELEROMETER = BNO055_ACCEL_DATA_X_LSB_ADDR,
+            VECTOR_MAGNETOMETER  = BNO055_MAG_DATA_X_LSB_ADDR,
+            VECTOR_GYROSCOPE     = BNO055_GYRO_DATA_X_LSB_ADDR,
+            VECTOR_EULER         = BNO055_EULER_H_LSB_ADDR,
+            VECTOR_LINEARACCEL   = BNO055_LINEAR_ACCEL_DATA_X_LSB_ADDR,
+            VECTOR_GRAVITY       = BNO055_GRAVITY_DATA_X_LSB_ADDR
+        }                    VectorType;
+
+        typedef enum {
+            NO_ERROR       = 0,
+            BEG_ERROR      = 256,
+            FAILED_TO_BOOT = BEG_ERROR,
+            END_ERROR      = FAILED_TO_BOOT
+        }                    ErrorCode;
+
+    public:
+        BNO055 (const uint8_t address = ADDRESS_A, const I2CMaster &bus = pwI2c)
+            : m_bus(&bus),
+              m_address(address) {
+        }
+
+        PropWare::ErrorCode begin (OperationMode mode = OPERATION_MODE_NDOF) const {
+            /* Make sure we have the right device */
+            auto id = pwI2c.get(this->m_address, BNO055_CHIP_ID_ADDR);
+            if (id != ID) {
+                waitcnt(SECOND + CNT); // hold on for boot
+                id = pwI2c.get(this->m_address, BNO055_CHIP_ID_ADDR);
+                if (id != ID) {
+                    return FAILED_TO_BOOT;  // still not? ok bail
+                }
+            }
+
+            /* Switch to config mode (just in case since this is the default) */
+            setMode(OPERATION_MODE_CONFIG);
+
+            /* Reset */
+            this->m_bus->put(this->m_address, BNO055_SYS_TRIGGER_ADDR, 0x20);
+            while (this->m_bus->get(this->m_address, BNO055_CHIP_ID_ADDR) != ID) {
+                waitcnt(10 * MILLISECOND + CNT);
+            }
+            waitcnt(50 * MILLISECOND + CNT);
+
+            /* Set to normal power mode */
+            this->m_bus->put(this->m_address, BNO055_PWR_MODE_ADDR, POWER_MODE_NORMAL);
+            waitcnt(10 * MILLISECOND + CNT);
+
+            this->m_bus->put(this->m_address, BNO055_PAGE_ID_ADDR, 0);
+
+            /* Set the output units */
+            /*
+            uint8_t unitsel = (0 << 7) | // Orientation = Android
+                              (0 << 4) | // Temperature = Celsius
+                              (0 << 2) | // Euler = Degrees
+                              (1 << 1) | // Gyro = Rads
+                              (0 << 0);  // Accelerometer = m/s^2
+            write8(BNO055_UNIT_SEL_ADDR, unitsel);
+            */
+
+            /* Configure axis mapping (see section 3.4) */
+            /*
+            write8(BNO055_AXIS_MAP_CONFIG_ADDR, REMAP_CONFIG_P2); // P0-P7, Default is P1
+            delay(10);
+            write8(BNO055_AXIS_MAP_SIGN_ADDR, REMAP_SIGN_P2); // P0-P7, Default is P1
+            delay(10);
+            */
+
+            this->m_bus->put(this->m_address, BNO055_SYS_TRIGGER_ADDR, 0x0);
+            waitcnt(10 * MILLISECOND + CNT);
+
+            /* Set the requested operating mode (see section 3.3) */
+            setMode(mode);
+            waitcnt(20 * MILLISECOND + CNT);
+
+            return NO_ERROR;
+        }
+
+        void setMode (OperationMode mode) const;
+        void getRevInfo (RevisionInfo *) const;
+        void displayRevInfo (void) const;
+        void setExtCrystalUse (bool usextal) const;
+        void getSystemStatus (uint8_t *system_status,
+                              uint8_t *self_test_result,
+                              uint8_t *system_error) const;
+        void displaySystemStatus (void) const;
+        void getCalibration (uint8_t *system, uint8_t *gyro, uint8_t *accel, uint8_t *mag) const;
+
+        imu::Vector<3> getVector (VectorType vector_type) const;
+        imu::Quaternion getQuat (void) const;
+        int8_t getTemp (void) const;
+
+        /* Functions to deal with raw calibration data */
+        bool getSensorOffsets (uint8_t *calibData) const;
+        bool getSensorOffsets (Offsets &offsets_type) const;
+        void setSensorOffsets (const uint8_t *calibData) const;
+        void setSensorOffsets (const Offsets &offsets_type) const;
+        bool isFullyCalibrated (void) const;
+
+    private:
+        const I2CMaster *m_bus;
+        const uint8_t   m_address;
+};
+
+}
